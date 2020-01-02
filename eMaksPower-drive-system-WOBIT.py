704b23d835e938786e7a45682d490b2c074070a8
Hubert <33498374+geodeta1997@users.noreply.github.com>
2020-01-02 17:24:26 +0100

    fix many problems with noises

--- eMaksPower-drive-system-WOBIT.py
+++ eMaksPower-drive-system-WOBIT.py
@@ -9,7 +9,7 @@
 #* Company  : Ko³o Naukowe Robotyków                                         *#
 #*                                                                           *#
 #* Author   : Hubert Graczyk / Piotr Saffarini                               *#
-#* Date     : 25.10.2019                                                     *#
+#* Date     : 29.11.2019                                                     *#
 #*===========================================================================*#
 
 DefUserVar(name="x_dir", value = 1, descr ="", min_value=0x00, max_value =0xFFFF)# X DIR - 0x5101.01h
@@ -27,30 +27,39 @@ def InitPars():
    Sp(0x3000, 0x00, 0x1)              # DEV_Cmd - Clear error
    Sp(0x3000, 0x00, 0x82)             # DEV_Cmd - Default parameter
 
-   Sp(0x3221, 0x00, 20000)            # CURR_LimitMaxPos
-   Sp(0x3223, 0x00, 20000)            # CURR_LimitMaxNeg
-   Sp(0x3224, 0x01, 30000)            # CURR_DynLimitPeak
-   Sp(0x3224, 0x02, 5000)             # CURR_DynLimitCont
+   # Current Limits
+   Sp(0x3221, 0x00, 35000)            # CURR_LimitMaxPos
+   Sp(0x3223, 0x00, 50000)            # CURR_LimitMaxNeg
 
-   Sp(0x3240, 0x00, 10000)            # CURR_Acc_dI
-   Sp(0x3241, 0x00, 10000)            # CURR_Acc_dT
-   Sp(0x3242, 0x00, 10000)            # CURR_Dec_dI
-   Sp(0x3243, 0x00, 10000)            # CURR_Dec_dT
-   Sp(0x3244, 0x00, 40000)            # CURR_Dec_QuickStop_dI
-   Sp(0x3245, 0x00, 50)               # CURR_Dec_QuickStop_dT
+   # Current Ramp deactivate
+   #Sp(0x324C, 0x00, 0)                # CURR_RampType deactivate
 
-   Sp(0x324C, 0x00, 1)                # CURR_RampType
+   '''
+   # Current Ramp activate
+   Sp(0x324C, 0x00, 1)                # CURR_RampType activate
+
+   # Acceleration ramp = 4000[mA] / 1000[ms]
+   Sp(0x3240, 0x00, 4000)             # CURR_Acc_dI
+   Sp(0x3241, 0x00, 1000)             # CURR_Acc_dT
+
+   # Deceleration ramp = 4000[mA] / 1000[ms]
+   Sp(0x3242, 0x00, 4000)             # CURR_Dec_dI
+   Sp(0x3243, 0x00, 1000)             # CURR_Dec_dT
+
+   # QuickStop ramp = 20000[mA] / 100[ms]
+   Sp(0x3244, 0x00, 20000)            # CURR_Dec_QuickStop_dI
+   Sp(0x3245, 0x00, 100)              # CURR_Dec_QuickStop_dT
+   '''
 
    Sp(0x3350, 0x00, 0x94A)            # VEL_Feedback
-
    Sp(0x3550, 0x00, 0x94A)            # SVEL_Feedback
 
    Sp(0x3830, 0x00, 32000)            # PWM_Frequency
 
+   # Motor parameters ------------------------------------------------------
    Sp(0x3900, 0x00, 1)                # MOTOR_Type
    Sp(0x3901, 0x00, 3000)             # MOTOR_Nn
    Sp(0x3902, 0x00, 48000)            # MOTOR_Un
-
    Sp(0x3910, 0x00, 8)                # MOTOR_PolN
    Sp(0x3911, 0x00, 2)                # MOTOR_Polarity
 
@@ -58,32 +67,44 @@ def InitPars():
    Sp(0x3003, 0x00, 3)                # DEV_Mode - VEL mode
    Sp(0x3304, 0x00, 0x0300)           # Enable Velocity from 0x3300 register
    Sp(0x3300, 0x00, 0)                # Velocity = 0 RPM
-   Sp(0x334C, 0x00, 0)                # Deactivate the ramp generator
 
-   Sp(0x3321, 0x00, 800)              # VEL_LimitMaxPos - pos. limit = 2000
-   Sp(0x3323, 0x00, -800)             # VEL_LimitMaxNeg - neg. limit = 2000
+   # Velocity Limits
+   Sp(0x3321, 0x00, 1200)             # VEL_LimitMaxPos - pos. limit = 1000
+   Sp(0x3323, 0x00, -600)            # VEL_LimitMaxNeg - neg. limit = -1000
 
-   '''
+   # Velocity Ramp deactivate
+   #Sp(0x334C, 0x00, 0)               # Deactivate the ramp generator
+
+   # Velocity Ramp activate
    Sp(0x334C, 0x00, 1)                # Activate the ramp generator
-   Sp(0x3340, 0x00, 2000)             # Acceleration_dV = 2000 RPM
-   Sp(0x3341, 0x00, 100)              # Acceleration_dT = 100 s
+
+   # Acceleration ramp = 1000[RPM] / 5000[ms]
+   Sp(0x3340, 0x00, 1000)             # Acceleration_dV = 1000 RPM
+   Sp(0x3341, 0x00, 5000)             # Acceleration_dT = 5000 ms
+
+   # Deceleration ramp = 1000[RPM] / 3000[ms]
    Sp(0x3342, 0x00, 1000)             # Deceleration_dV = 1000 RPM
-   Sp(0x3343, 0x00, 200)              # Deceleration_dT = 200 s
-   '''
+   Sp(0x3343, 0x00, 1000)             # Deceleration_dT = 1000 ms
+
+   # QuickStop deceleration = 3000[RPM] / 500[ms]
+   Sp(0x3344, 0x00, 3000)             # VEL_Dec_QuickStop_dV = 3000 RPM
+   Sp(0x3345, 0x00, 100)              # VEL_Dec_QuickStop_dT = 500 ms
+
+
+   # Velocity and Current regulators ------------------------------------------
 
    # PID Velocity regulator
-   Sp(0x3310, 0x00, 6)                # VEL_Kp - Kp = 6
+   Sp(0x3310, 0x00, 1500)             # VEL_Kp - Kp = 6
    Sp(0x3311, 0x00, 0)                # VEL_Ki - Ki = 0
-   Sp(0x3312, 0x00, 1000)             # VEL_Kd - Kd = 1000 
-   
+   Sp(0x3312, 0x00, 0)                # VEL_Kd - Kd = 1000
+
    # PI Current regulator
-   Sp(0x3210, 0x00, 8)                # CURR_Kp - set factor Kp of the current controller
+   Sp(0x3210, 0x00, 50)               # CURR_Kp - set factor Kp of the current controller
    Sp(0x3211, 0x00, 32)               # CURR_Ki - set factor Ki of the current controller
 
    Sp(0x3004, 0x00, 1)                # DEV_Enable - Enable
 
 # Configuration of CAN frames -------------------------------------------------
-
    Sp(0x2011, 0x02, 1684107116)       # ...tion - Default parameter communication
    Sp(0x2011, 0x05, 1684107116)       # DS2000_RestoreD...000 - Default parameter
 
@@ -103,20 +124,20 @@ def InitPars():
 # ===== RX FRAME DATA ===== #
    #0x21D - AXIS X
    Sp(0x1600, 0x00, 0x0)              # Disable mapping
-   Sp(0x1600, 0x01, 0x51010210)       # object 0: JOY_ADC_X AXIS Value % (2 bytes)
+   Sp(0x1600, 0x01, 0x51010210)       # object 0: JOY_ADC_X AXIS Value   (2 bytes)
    Sp(0x1600, 0x02, 0x51010110)       # object 1: DIR: 1-Forward, 0-Rear (2 byte)
    Sp(0x1600, 0x00, 0x2)              # Enable mapping with 3 objects
 
    #0x22D - AXIS Y
    Sp(0x1601, 0x00, 0x0)              # Disable mapping
-   Sp(0x1601, 0x01, 0x51020210)       # object 0: JOY_ADC_Y AXIS Value % (2 bytes)
+   Sp(0x1601, 0x01, 0x51020210)       # object 0: JOY_ADC_Y AXIS Value   (2 bytes)
    Sp(0x1601, 0x02, 0x51020110)       # object 1: DIR: 1-Forward, 0-Rear (2 byte)
    Sp(0x1601, 0x00, 0x2)              # Enable mapping with 3 objects
 
    #0x30D - LEDs State
    Sp(0x1602, 0x00, 0x0)              # Disable mapping
    Sp(0x1602, 0x01, 0x31580008)       # object 0: LED Enable (1 bytes)
-   Sp(0x1602, 0x02, 0x31580108)       # object 1: LED State (1 bytes)
+   Sp(0x1602, 0x02, 0x31580108)       # object 1: LED State  (1 bytes)
    Sp(0x1602, 0x00, 0x2)              # Enable mapping with 2 objects
 
    # ===== TX CAN CONFIG ===== #
@@ -166,25 +187,33 @@ Sp(0x2040,0x02,5)                     # NMT communication Enable
 # Main loop -------------------------------------------------------------------
 while 1:
    x_precent_value = (x_axis_value*128)/65535  #precentage conversion
-   '''
-   if(x_dir == 1):
-      Sp(0x3300, 0x00, 2*x_precent_value)
 
-   elif(x_dir == 0):
-      Sp(0x3300, 0x00, -2*x_precent_value)
-   '''
+   # Drive forward
+   if(x_dir == 1 and (x_precent_value > 10)):
+     Sp(0x3004, 0x00, 1)                # DEV_Enable - Enable
+     Sp(0x3300, 0x00, (115*x_precent_value/100 * Gp(0x3323, 0x00)/100))
+
+   # Drive backwards
+   elif(x_dir == 0 and (x_precent_value > 10)):
+     Sp(0x3004, 0x00, 1)                # DEV_Enable - Enable
+     Sp(0x3300, 0x00, (130*x_precent_value/100 * Gp(0x3321, 0x00)/100))
+
+   # Stop
+   #elif(x_precent_value < 15 and (Gp(0x3361, 0x00)<50 and Gp(0x3361, 0x00)>-50)):  
+   elif(x_precent_value < 10 and Gp(0x3361, 0x00)<50 and Gp(0x3361, 0x00)>-50):
+     Sp(0x3300, 0x00, 0)
+     Sp(0x3004, 0x00, 0)                # DEV_Enable - Disable
 
 
-   #Przeskalowane procentowo
-   if(x_dir == 1 and x_precent_value):
-     Sp(0x3300, 0x00, (x_precent_value * Gp(0x3321, 0x00)/100))
-
-   elif(x_dir == 0 and x_precent_value >=30):
-     Sp(0x3300, 0x00, (x_precent_value * Gp(0x3323, 0x00)/100))
-     
-   else:
-     Sp(0x3300, 0x00, 0) 
-
+   #===== TO DO =====#
+   #                 #
+   # QuickStop Event #
+   #                 #
+   #     aktywne     #
+   #    hamowanie    #
+   #    silnikiem    #
+   #                 #
+   #=================#
 
 
 
